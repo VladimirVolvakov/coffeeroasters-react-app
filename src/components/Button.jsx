93120f3e7a3e6987c633ebbe1f1c31e1748125aa
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-10 00:12:01 +0300

    fix: fix bug of className choice in Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,10 @@ import styles from "./Button.module.css";
 const Button = ({ children, isDisabled }) => {
   return (
     <button
-      className={styles.btn}
+      className={`${styles.btn} ${
+        isDisabled ? `${styles["btn-disabled"]}` : ""
+      }`}
+      disabled={isDisabled}
     >
       {children}
     </button>
