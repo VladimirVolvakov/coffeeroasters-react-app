482c52d1f3a79e8aeba5952bbd05bbba71d3f618
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-12 23:23:38 +0300

    style: add styles for SectionHeroHome component

--- src/layout/SectionHeroHome.jsx
+++ src/layout/SectionHeroHome.jsx
@@ -1,12 +1,13 @@
 import Button from "../components/Button";
+import "../index.css";
 import "./SectionHeroHome.css";
 
 const SectionHeroHome = () => {
   return (
-    <section>
+    <section className="hero">
       <div>
-        <h1>Great coffee made simple.</h1>
-        <p>
+        <h1 className="heading-xl hero__heading">Great coffee made simple.</h1>
+        <p className="font-body hero__subheading">
           Start your mornings with the world's best coffees. Try our expertly
           curated artisan coffees from our best roasters delivered directly to
           your door, at your schedule.
