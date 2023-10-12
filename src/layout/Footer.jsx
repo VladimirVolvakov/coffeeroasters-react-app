1960b041334f3585cc1db76152ef167fe3bd5f8c
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-12 19:00:58 +0300

    style: add styles for Footer component

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,40 +1,52 @@
+import "../index.css";
+import "./Footer.css";
+
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer">
       <img
         src="assets/shared/desktop/logo-light.svg"
         alt="Coffeeroasters' logo"
       />
-      <nav>
-        <ul>
+      <nav className="footer__nav">
+        <ul className="footer__nav-menu">
           <li>
-            <a href="/index.html">Home</a>
+            <a href="/index.html" className="font-nav footer__nav-link">
+              Home
+            </a>
           </li>
           <li>
-            <a href="/about-us.html">About us</a>
+            <a href="/about-us.html" className="font-nav footer__nav-link">
+              About us
+            </a>
           </li>
           <li>
-            <a href="/create-plan.html">Create your plan</a>
+            <a href="/create-plan.html" className="font-nav footer__nav-link">
+              Create your plan
+            </a>
           </li>
         </ul>
       </nav>
-      <div>
+      <div className="footer__media-links">
         <a href="https://www.facebook.com">
           <img
             src="assets/shared/desktop/icon-facebook.svg"
             alt="Our Facebook account"
+            className="footer__media-img"
           />
         </a>
         <a href="https://www.twitter.com">
           <img
             src="assets/shared/desktop/icon-twitter.svg"
             alt="Our Twitter account"
+            className="footer__media-img"
           />
         </a>
         <a href="https://www.instagram.com">
           <img
             src="assets/shared/desktop/icon-instagram.svg"
             alt="Our Instagram account"
+            className="footer__media-img"
           />
         </a>
       </div>
