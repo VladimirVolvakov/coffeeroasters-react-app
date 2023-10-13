79f4c347c7ec36f376a00867b9ef346b61a0051b
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-13 23:34:46 +0300

    style: add styles for CollectionItem component

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,12 +1,13 @@
+import "../index.css";
 import "./CollectionItem.css";
 
 const CollectionItem = ({ image, title, description }) => {
   return (
-    <div>
-      <img src={image} alt={title} />
+    <div className="collection__card">
+      <img src={image} alt={title} className="collection__card-img" />
       <div>
-        <p>{title}</p>
-        <p>{description}</p>
+        <p className="heading-sm collection__card-title">{title}</p>
+        <p className="font-body collection__card-desc">{description}</p>
       </div>
     </div>
   );
