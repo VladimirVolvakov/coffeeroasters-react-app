9c4b4d2b6ace32c7ff2761321247a03964b44a14
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-16 23:18:16 +0300

    style: add styles for SectionFeatures component

--- src/layout/SectionFeatures.jsx
+++ src/layout/SectionFeatures.jsx
@@ -1,18 +1,19 @@
+import "../index.css";
 import "./SectionFeatures.css";
 import { featuresList } from "../data/indexPageData";
 import FeatureItem from "../components/FeatureItem";
 
 const SectionFeatures = () => {
   return (
-    <section>
-      <div>
-        <h2>Why choose us</h2>
-        <p>
+    <section className="features">
+      {/* <div> */}
+        <h2 className="heading-lg features__heading">Why choose us?</h2>
+        <p className="font-body features__subheading">
           A large part of our role is choosing which particular coffees will be
           featured in our range. This means working closely with the best coffee
           growers to give you a more impactful experience on every level.
         </p>
-        <div>
+        <div className="features__list">
           {featuresList.map((feature) => (
             <FeatureItem
               key={feature.title}
@@ -22,7 +23,7 @@ const SectionFeatures = () => {
             />
           ))}
         </div>
-      </div>
+      {/* </div> */}
     </section>
   );
 };
