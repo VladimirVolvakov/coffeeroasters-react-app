4ad37333cf54c8890b3e4abd71706df49fe6379b
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-16 23:58:31 +0300

    style: add styles for FeatureItem component

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,12 +1,13 @@
+import "../index.css";
 import "./FeatureItem.css";
 
-const FeatureItem = ({ image, title, description}) => {
+const FeatureItem = ({ image, title, description }) => {
   return (
-    <div>
+    <div className="feature__item">
       <img src={image} alt={title} />
-      <div>
-        <p>{title}</p>
-        <p>{description}</p>
+      <div className="feature__text">
+        <h3 className="heading-sm feature__title">{title}</h3>
+        <p className="font-body feature__desc">{description}</p>
       </div>
     </div>
   );
