5a87d31ee3336dbaf71aa82be7122fd500462b9d
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-22 21:36:02 +0300

    style: add styles for SectionAboutUs conponent

--- src/layout/SectionAboutUs.jsx
+++ src/layout/SectionAboutUs.jsx
@@ -1,9 +1,12 @@
+import "../index.css";
+import "./SectionAboutUs.css";
+
 const SectionAboutUs = () => {
   return (
     <section className="about-us">
-      <div>
-        <h1>About Us</h1>
-        <p>
+      <div className="about-us__text-container">
+        <h1 className="heading-lg about-us__heading">About Us</h1>
+        <p className="font-body about-us__subheading">
           Coffeeroasters began its journey of exotic discovery in 1999,
           highlighting stories of coffee from around the world. We have since
           been dedicated to bring the perfect cup - from bean to brew - in every
