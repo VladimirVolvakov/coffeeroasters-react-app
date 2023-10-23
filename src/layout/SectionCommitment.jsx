f5a53055d70e208b926765dfb52afe7108a56c60
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-23 15:41:06 +0300

    style: add styles for media queries <=1050px, <=900px, <=700px & <=550px for SectionCommitment component

--- src/layout/SectionCommitment.jsx
+++ src/layout/SectionCommitment.jsx
@@ -4,28 +4,28 @@ import "./SectionCommitment.css";
 const SectionCommitment = () => {
   return (
     <section className="commitment">
-        <img
-          src="./assets/about/desktop/image-commitment.jpg"
-          alt="Discover world's best coffee"
-          className="commitment__image"
-        />
-        <div className="commitment__text-container">
-          <h2 className="heading-lg commitment__heading">Our commitment</h2>
-          <p className="font-body commitment__description">
-            We’re built on a simple mission and a commitment to doing good along
-            the way. We want to make it easy for you to discover and brew the
-            world’s best coffee at home. It all starts at the source. To locate
-            the specific lots we want to purchase, we travel nearly 60 days a
-            year trying to understand the challenges and opportunities in each
-            of these places. We collaborate with exceptional coffee growers and
-            empower a global community of farmers through with well above
-            fair-trade benchmarks. We also offer training, support farm
-            community initiatives, and invest in coffee plant science. Curating
-            only the finest blends, we roast each lot to highlight tasting
-            profiles distinctive to their native growing region.
-          </p>
-        </div>
-      </section>
+      <img
+        src="./assets/about/desktop/image-commitment.jpg"
+        alt="Discover world's best coffee"
+        className="commitment__image"
+      />
+      <div className="commitment__text-container">
+        <h2 className="heading-lg commitment__heading">Our commitment</h2>
+        <p className="font-body commitment__description">
+          We’re built on a simple mission and a commitment to doing good along
+          the way. We want to make it easy for you to discover and brew the
+          world’s best coffee at home. It all starts at the source. To locate
+          the specific lots we want to purchase, we travel nearly 60 days a year
+          trying to understand the challenges and opportunities in each of these
+          places. We collaborate with exceptional coffee growers and empower a
+          global community of farmers through with well above fair-trade
+          benchmarks. We also offer training, support farm community
+          initiatives, and invest in coffee plant science. Curating only the
+          finest blends, we roast each lot to highlight tasting profiles
+          distinctive to their native growing region.
+        </p>
+      </div>
+    </section>
   );
 };
 
