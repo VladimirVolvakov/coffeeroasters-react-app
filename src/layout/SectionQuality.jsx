082c3a66a10486a3df241e515a69940f46d116d1
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-23 16:51:37 +0300

    style: add styles for SectionQuality conponent

--- src/layout/SectionQuality.jsx
+++ src/layout/SectionQuality.jsx
@@ -1,9 +1,12 @@
+import "../index.css";
+import "./SectionQuality.css";
+
 const SectionQuality = () => {
   return (
     <section className="quality">
-        <div>
-          <h2>Uncompromising quality</h2>
-          <p>
+        <div className="quality__text-container">
+          <h2 className="heading-lg quality__heading">Uncompromising quality</h2>
+          <p className="font-body quality__description">
             Although we work with growers who pay close attention to all stages
             of harvest and processing, we employ, on our end, a rigorous quality
             control program to avoid over-roasting or baking the coffee dry.
@@ -13,8 +16,9 @@ const SectionQuality = () => {
           </p>
         </div>
         <img
-          src="assets/about/desktop/image-quality.jpg"
+          src="./assets/about/desktop/image-quality.jpg"
           alt="Uncompromising quality of our coffee"
+          className="quality__image"
         />
       </section>
   );
