00a63dbd0f687b8c14d81dca18622bd65abc424c
VladimirVolvakov <dr.volvakov@gmail.com>
2023-10-28 20:37:51 +0300

    style: add styles for SectionHeroPlan conponents

--- src/layout/SectionHeroPlan.jsx
+++ src/layout/SectionHeroPlan.jsx
@@ -4,9 +4,9 @@ import "./SectionHeroPlan.css";
 const SectionHeroPlan = () => {
   return (
     <section className="hero-plan">
-      <div>
-        <h1>Create plan</h1>
-        <p>
+      <div className="hero-plan__text-container">
+        <h1 className="heading-xl hero-plan__heading">Create plan</h1>
+        <p className="font-body hero-plan__subheading">
           Coffee the way you wanted it to be. For coffee delivered tomorrow, or
           next week. For whatever brew method you use. For choice, for
           convenience, for quality.
